816de01ef87ff19d3f84ed51c401f762d7cc5056
opti0 <mcjrdck9@gmail.com>
2023-11-13 11:11:24 +0100

    Older projects and part of lab6

--- uklad1/uklad1.srcs/sources_1/new/uklad2.vhd
+++ uklad1/uklad1.srcs/sources_1/new/uklad2.vhd
@@ -6,20 +6,18 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use IEEE.NUMERIC_STD.ALL;
 
 entity uklad2 is
-    Port ( x1 : in STD_LOGIC;
-           x2 : in STD_LOGIC;
-           y1 : out STD_LOGIC;
-           y2 : out STD_LOGIC;
-           y3 : out STD_LOGIC;
-           y4 : out STD_LOGIC);
+    Port ( x : in std_logic_vector (1 downto 0);
+           y : out std_logic_vector (3 downto 0));
 
 end uklad2;
 
 architecture Behavioral of uklad2 is
-
+signal s0, s1 : std_logic;
 begin
-y4 <= x1 and x2;
-y3 <= x2 and not x1;
-y2 <= x1 and not x2;
-y1 <= not x1 and not x2;
+s0 <= not x(0);
+s1 <= not x(1);
+y(0) <= s0 and s1;
+y(1) <= s1 and x(0);
+y(2) <= s0 and x(1);
+y(3) <= x(0) and x(1);
 end Behavioral;
